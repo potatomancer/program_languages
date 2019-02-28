acb0d49d924a4f0eee71962640923ce6bbe62fe7
Joshua Wilkes <joshuaryanw@gmail.com>
2019-02-28 11:26:06 -0600

    register attempt FUCKING 25

--- jwilkes-p2/parser.c
+++ jwilkes-p2/parser.c
@@ -387,7 +387,7 @@ void evaluateTokenForPostfix()
     6. Repeat steps 2-6 until infix expression is scanned.
     7. Print the output
     8. Pop and output from the stack until it is not empty. */
-  if (strcmp(runner->next->word, ";") == 0 && getBufferLast() != NULL)
+  if (runner->next != NULL && strcmp(runner->next->word, ";") == 0 && getBufferLast() != NULL)
   {
     printf("idk what I'm doing\n");
     pushToRegister(popFromBuffer());
