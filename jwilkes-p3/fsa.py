237d9d9043ea63d220db6ee821f2c0652b5e73f0
Joshua Wilkes <joshuaryanw@gmail.com>
2019-03-05 21:44:43 -0600

    Just about damn finished project 3

--- jwilkes-p3/fsa.py
+++ jwilkes-p3/fsa.py
@@ -1,4 +1,5 @@
 from tkinter import Tk, Canvas, LAST, mainloop
+import math
 
 import sys
 
@@ -7,14 +8,12 @@ if len(sys.argv) == 3:
     try:
         with open(sys.argv[1]) as firstFile:
             firstContent = firstFile.read()
-            print(firstContent)
     except EnvironmentError:
         print("Invalid first argument passed.")
         quit()
     try:
         with open(sys.argv[2]) as secondFile:
             secondContent = secondFile.read()
-            print(secondContent)
     except EnvironmentError:
         print("Invalid second argument passed.")
         quit()
@@ -25,8 +24,22 @@ if len(sys.argv) == 3:
         number_of_states = int(fsa[0])
         alphabet = fsa[1]
         state_transitions = fsa[2].split(",")
+        # Iterate through state_transitions list
+        for i, trans in enumerate(state_transitions):
+            # Left and right strip the paranthesis from the state_transitions list
+            trans = trans.lstrip("(")
+            trans = trans.rstrip(")")
+            # Split each transition on colon
+            trans = trans.split(":")
+            # Iterate through the resultant array
+            for index in range(3):
+                # Make the first two values integers, keeping the third as a string
+                if index != 2:
+                    trans[index] = int(trans[index])
+            state_transitions[i] = trans
         start_state = int(fsa[3])
         accept_states = fsa[4].split(",")
+        pathString = list(map(str, secondContent))
     except ValueError:
         print("Invalid FSA file.")
         quit()
@@ -35,41 +48,145 @@ if len(sys.argv) == 3:
     circle_diameter = 50
     line_length = 100
     canvas_title = "Computer Programming Project 3 - FSA GUI"
-    canvas_width = 350
+    # canvas_width gets set at the bottom
+    canvas_width = 500
     canvas_height = 50 + ((circle_diameter * number_of_states) +
                           (number_of_states - 1) * line_length)
     master = Tk()
     canvas = Canvas(master, width=canvas_width,
                     height=canvas_height, bg="white")
-
     # x and y coordinates to construct graph with
     x = canvas_width/2
     y = 25
-
-    #nodes and links
+    # nodes and links
     states = []
     links = []
 
-    # Create first state
-    states.append(canvas.create_oval(
-        x - (circle_diameter/2), y, x + (circle_diameter/2), y + circle_diameter, fill="lightblue"))
-    canvas.create_text(x, y + circle_diameter/2, text=0,
-                       font="Arial 20")
-    # Create every other state
-    for index in range(number_of_states - 1):
-        y = y + circle_diameter
-        links.append(canvas.create_line(
-            x, y, x, y + line_length, width=2, arrow=LAST))
-        y = y + line_length
-        states.append(canvas.create_oval(
-            x - (circle_diameter/2), y, x + (circle_diameter/2), y + circle_diameter, fill="lightblue"))
-        canvas.create_text(x, y + circle_diameter/2, text=index + 1,
-                           font="Arial 20")
+    # Create every state
+    for index in range(number_of_states):
+        if index == 0:
+            # Create first state
+            states.append(canvas.create_oval(
+                x - (circle_diameter/2),
+                y,
+                x+(circle_diameter/2),
+                y + circle_diameter, fill="lightblue"))
+            canvas.create_text(x, y + circle_diameter/2,
+                               text=index+start_state, font="Arial 20")
+        else:
+            # Fix height of y to the bottom of the circle
+            y = y + circle_diameter + line_length
+            # Create new circle and append it to the states list
+            states.append(canvas.create_oval(
+                x-(circle_diameter/2),
+                y,
+                x+(circle_diameter/2),
+                y+circle_diameter, fill="lightblue"))
+            # Create state text and place it in the center of the circle, Arial 20 because I like to style things don't @ me
+            canvas.create_text(x, y + circle_diameter/2, text=index + start_state,
+                               font="Arial 20")
 
-    for state in states:
-        print(canvas.coords(state)[0], canvas.coords(
-            state)[1], canvas.coords(state)[2], canvas.coords(state)[3], )
+    # Reset y to canvas padding from top + first circle
+    y = 25 + circle_diameter
 
+    # Create variable for measuring how many multiple state transitions (MST) happen and a variable for the padding
+    mst_number = 0
+    mst_padding = 25
+    # Create every state transition per state
+    for index in range(number_of_states):
+        transitions = filter(lambda x: x[0] == index, state_transitions)
+        for z in transitions:
+            # From current state to same state
+            if z[1] == index:
+                # Place x, y position at center of current circle
+                y = y - circle_diameter/2
+                # Create line that curves out from circle and points back to circle
+                links.append(canvas.create_line(
+                    x+(circle_diameter/2)*math.sin(math.pi/4),
+                    y+(circle_diameter/2)*math.cos(math.pi/4),
+                    x+circle_diameter*1.5,
+                    y,
+                    x+(circle_diameter/2)*math.sin(3*math.pi/4),
+                    y+(circle_diameter/2)*math.cos(3*math.pi/4), width=2, arrow=LAST, smooth=True))
+                # Give line its label
+                canvas.create_text(x + 7 + circle_diameter, y, text=z[2],
+                                   font="Arial 20")
+                # Reset y value back to bottom of the circle
+                y = y + circle_diameter/2
+            # From current state to next state:
+            elif z[1] == index + 1:
+                # Create new line, append it to the links list, and update y
+                links.append(canvas.create_line(
+                    x, y, x, y + line_length, width=2, arrow=LAST))
+                y = y + line_length/2
+                canvas.create_text(x + 10, y, text=z[2],
+                                   font="Arial 20")
+                y = y + line_length/2 + circle_diameter
+            # From current state jumping to state more than one away
+            elif z[1] <= number_of_states:
+                # Increment multiple state transition number
+                mst_number = mst_number + 1
+                # Store x, y position
+                oldX = x
+                oldY = y
+                # Set y to center of state circle
+                y = y-(circle_diameter/2)
+                # Set new x, y to the left side of the state circle
+                x = x-(circle_diameter/2)*math.sin(math.pi/2)
+                y = y-(circle_diameter/2)*math.cos(math.pi/2)
+                # Create line that extends out mst_padding * mst_number worth of times
+                links.append(canvas.create_line(
+                    x, y, x-(mst_padding*mst_number), y, width=2))
+                # Update x to the end of the line
+                x = x-(mst_padding*mst_number)
+                # Get the difference of states between current and target state
+                difference_states = z[1] - z[0]
+                # Create a line that extends up or down to the target state
+                links.append(canvas.create_line(
+                    x, y, x, y+((circle_diameter+line_length)*difference_states), width=2))
+                # Update y to the end of the line
+                y = y+((circle_diameter+line_length)*difference_states)
+                # Create label for arrow here
+                canvas.create_text(x + 10, y-(((circle_diameter+line_length)*difference_states)/2), text=z[2],
+                                   font="Arial 20")
+                # Create a line that points back to the target state
+                links.append(canvas.create_line(
+                    x, y, x+(mst_padding*mst_number), y, width=2, arrow=LAST))
+                # Reset x, y position back to the bottom of the circle of the current state
+                x = oldX
+                y = oldY
+    # Now parse through the pathString to determine if the path is legal with currentNodeInPath starting at 0
+    currentNodeInPath = 0
+    legalPath = True
+    for path in pathString:
+        # If current character in pathString is a newline, space, or tab, just continue
+        if path == "\n" or path == " " or path == "\t":
+            continue
+        # If current character in pathString is found (not -1) in the valid path variables (alphabet)
+        if(alphabet.find(path) != -1):
+            # Return an array of the possible path options from state_transitions
+            pathOptions = filter(
+                lambda x: x[0] == currentNodeInPath, state_transitions)
+            # In each state transition
+            for pathOption in pathOptions:
+                # If current character in pathString finds an acceptable pathj
+                if path == pathOption[2]:
+                    # Then take that path, and make sure legalPath is True and break from inner for loop
+                    currentNodeInPath = pathOption[1]
+                    legalPath = True
+                    break
+                else:
+                    # Else, make sure legalPath remains false
+                    legalPath = False
+        # If legalPath was found false at the end of each 'path' loop, break loop
+        if legalPath == False:
+            break
+    # Print result of path
+    if legalPath:
+        print("Path is legal!")
+    else:
+        print("Path is not legal :(")
+    # Pack it up, title it up, loop it up!
     canvas.pack()
     master.wm_title(canvas_title)
     mainloop()
